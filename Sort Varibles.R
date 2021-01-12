43f02a62be0c340534e26004e0941bbd5d2e679d
Gemma <74992040+LadyHolz@users.noreply.github.com>
2021-01-12 22:15:04 +0000

    Add files via upload

---
+++ Sort Varibles.R
@@ -0,0 +1,11 @@
+uk.cities <-c("West Midlands" = "Birmingham", "East Midlands" = "Nottingham", 
+                 "West Yorkshire" = "Leeds")
+print(uk.cities)
+
+new.city <-("East Anglia" = "Norfolk")
+
+uk.cities <- c(uk.cities, new.city)
+
+print(uk.cities)
+
+ls()
